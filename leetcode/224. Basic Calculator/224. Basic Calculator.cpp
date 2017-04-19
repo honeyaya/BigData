02532fb8added2f0a1f608cd1146788f357915b4
honeyaya <1194866931@qq.com>
2017-04-19 17:03:31 +0800

    about string use stack

--- leetcode/224. Basic Calculator/224. Basic Calculator.cpp
+++ leetcode/224. Basic Calculator/224. Basic Calculator.cpp
@@ -1,6 +1,42 @@
+// ³Ë³ý²»¿ÉÒÔÇ¶Ì×£¬À¨ºÅ¿ÉÒÔÇ¶Ì×
+ 
 class Solution {
 public:
+	bool isdigit(char c){
+		return c>='0' && c<='9';
+	}
+	
     int calculate(string s) {
+        int len = s.length();
+        int sign = 1;
         
+        stack<long long> st;
+        long long ans = 0;
+		for(int i = 0; i < len; i++){
+        	if(isdigit(s[i])){
+        		long long tmp = s[i] - '0';
+        		while(i + 1 < len && isdigit(s[i+1])){
+        			tmp = tmp * 10 + s[i+1] - '0';
+        			i++;
+				}
+				
+				ans += tmp * sign;
+			} else if(s[i] == '+'){
+				sign = 1;
+			} else if(s[i] == '-'){
+				sign = -1;
+			} else if(s[i] == '('){
+				st.push(ans);
+				ans = 0;
+				st.push(sign);
+				sign = 1;
+			} else if(s[i] == ')'){
+				ans = st.top() * ans;
+				st.pop();
+				ans += st.top();
+				st.pop();
+			}
+		}
+		return ans;
     }
 };
--- leetcode/227. Basic Calculator II/227. Basic Calculator II.cpp
+++ leetcode/227. Basic Calculator II/227. Basic Calculator II.cpp
@@ -2,7 +2,46 @@
 
 class Solution {
 public:
+	bool isdigit(char c){
+		return c>='0' && c<='9';
+	}
+	
     int calculate(string s) {
-        
+        stack<int> st;
+        char sign = '+';
+        int tmp = 0;
+        int len = s.length();
+        for(int i = 0; i < len;i++){
+        	if(isdigit(s[i])){
+        		tmp = tmp * 10 + s[i] - '0';
+			}
+			if(!isdigit(s[i]) && (s[i] != ' ') || (i == len - 1)){
+				if(sign == '-')
+					st.push(-tmp);
+				else if(sign == '+'){
+					st.push(tmp);
+				}
+				else{
+					int num;
+					if(sign == '*'){
+						num = st.top() * tmp;
+					} else{
+						num = st.top() /tmp;
+					}
+					st.pop();
+					st.push(num);
+				}
+				sign = s[i];
+				tmp = 0;
+			} 
+		}
+		int ans = 0;
+		while(!st.empty()){
+			ans += st.top();
+			st.pop();
+		}
+		return ans;
     }
 };
+
+
